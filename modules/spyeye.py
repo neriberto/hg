72081d17b1ff3d363f80c120579e958e0ffec7a9
Neriberto C.Prado <neriberto.prado@bluepex.com>
2013-11-25 23:21:38 -0200

    Fix for Spyeye that brings 502 status code

--- modules/spyeye.py
+++ modules/spyeye.py
@@ -12,29 +12,22 @@ class spyeye(object):
 
     def run(self):
         URLS = []
-        # Download list from Zeus tracker
-        content = self.Download("https://spyeyetracker.abuse.ch/monitor.php?urlfeed=binaryurls")
-        if content != None:
-            bk = content.replace("\n","")
-            dom = parseString(bk.encode("utf-8"))
-            for node in dom.getElementsByTagName("title"):
-                # extract the URLs
-                line = node.toxml().replace("<title>", "")
-                info = line.replace("</title>", "")
-                try:
-                    URLS.append("http://" + info.split(',')[0].split(':')[0].split(' ')[0])
-                except:
-                    pass
-        URLS.pop(0)
-        return URLS
-
-    def Download(self, URL):
-        '''
-        :description : Execute download from a URL
-        '''
         try:
-            r = requests.get(URL)
-            return r.content
-        except Exception, e:
-            print "Failure, %s" % e
-            return None
+            r = requests.get("https://spyeyetracker.abuse.ch/monitor.php?urlfeed=binaryurls")
+            content = r.content
+            if content != None:
+                bk = content.replace("\n","")
+                dom = parseString(bk.encode("utf-8"))
+                for node in dom.getElementsByTagName("title"):
+                    # extract the URLs
+                    line = node.toxml().replace("<title>", "")
+                    info = line.replace("</title>", "")
+                    try:
+                        URLS.append("http://" + info.split(',')[0].split(':')[0].split(' ')[0])
+                    except:
+                        pass
+            URLS.pop(0)
+            return URLS
+        except Exception:
+            # Sometimes Spyeye give us 502 status code :)
+            pass
