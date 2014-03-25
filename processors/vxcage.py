52fd942c2b2f1179d51843e781e4a985f7b81688
Neriberto C.Prado <neriberto.prado@bluepex.com>
2014-03-25 11:23:25 -0300

    Working with try/except in vxcage

--- processors/vxcage.py
+++ processors/vxcage.py
@@ -11,20 +11,27 @@ import requests
 class vxcage (object):
 
     def run(self, CONFIG, MD5, FULLPATH, FILETYPE):
-        if CONFIG['VxCage']['Enabled'] == "yes":
-            if self.not_exist(MD5, CONFIG['VxCage']['connection']):
-                return self.add(CONFIG["VxCage"]['connection'], FULLPATH)
+        try:
+            if CONFIG['VxCage']['Enabled'] == "yes":
+                if self.not_exist(MD5, CONFIG['VxCage']['connection']):
+                    return self.add(CONFIG["VxCage"]['connection'], FULLPATH)
+        except Exception, e:
+            print "VxCage:run %s" % (e)
 
     def not_exist(self, MD5, URL):
-        Data = {'md5': MD5}
         try:
+            Data = {'md5': MD5}
             r = requests.post("%smalware/find" % URL, data=Data)
             return (r.status_code == 404)
-        except:
+        except Exception, e:
+            print "VxCage:not_exist %s" % (e)
             return False
 
     def add(self, URL, fullpath):
-        Files = {'file': (os.path.basename(fullpath), open(fullpath, 'rb'))}
-        r = requests.post("%smalware/add" % URL, files=Files)
-        # Returns 200 upload sucessfull, so remove
-        return (r.status_code == 200)
+        try:
+            Files = {'file': (os.path.basename(fullpath), open(fullpath, 'rb'))}
+            r = requests.post("%smalware/add" % URL, files=Files)
+            return (r.status_code == 200)
+        except Exception, e:
+            print "VxCage:add %s" % (e)
+            return False
