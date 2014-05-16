32dd30d3d19d7a10bc06aa1512bbd4b31af3d214
Neriberto C.Prado <neriberto.prado@bluepex.com>
2014-05-16 13:49:35 -0300

    This fixes #9 (Viper implementation)

--- processors/viper.py
+++ processors/viper.py
@@ -10,18 +10,28 @@ import requests
 
 class viper (object):
 
+    TYPES = []
+
+    def __init__(self):
+        self.TYPES.append("application/x-dosexec")
+        self.TYPES.append("application/x-executable")
+        self.TYPES.append("application/pdf")
+        self.TYPES.append("application/msword")
+        self.TYPES.append("application/octet-stream")
+
     def run(self, CONFIG, MD5, FULLPATH, FILETYPE):
         try:
             if CONFIG['Viper']['Enabled'] == "yes":
-                if self.not_exist(MD5, CONFIG['Viper']['connection']):
-                    return self.add(CONFIG["Viper"]['connection'], FULLPATH)
+                if FILETYPE in self.TYPES:
+                    if self.not_exist(MD5, CONFIG['Viper']['connection']):
+                        return self.add(CONFIG["Viper"]['connection'], FULLPATH)
         except Exception, e:
             print "Viper:run %s" % (e)
 
     def not_exist(self, MD5, URL):
         try:
             Data = {'md5': MD5}
-            r = requests.post("%smalware/find" % URL, data=Data)
+            r = requests.post("%sfile/find" % URL, data=Data)
             return (r.status_code == 404)
         except Exception, e:
             print "Viper:not_exist %s" % (e)
@@ -30,7 +40,7 @@ class viper (object):
     def add(self, URL, fullpath):
         try:
             Files = {'file': (os.path.basename(fullpath), open(fullpath, 'rb'))}
-            r = requests.post("%smalware/add" % URL, files=Files)
+            r = requests.post("%sfile/add" % URL, files=Files)
             return (r.status_code == 200)
         except Exception, e:
             print "Viper:add %s" % (e)
