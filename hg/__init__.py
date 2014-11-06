7f99bd00a8470bb9248ecbfb188ce2b775d05e34
Neriberto C.Prado <neriberto.prado@bluepex.com>
2014-11-06 09:30:47 -0200

    Reformat hg core according to PEP-8

--- hg/__init__.py
+++ hg/__init__.py
@@ -1,4 +1,6 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
+
 #
 
 import ConfigParser
@@ -15,6 +17,7 @@ import requests
 import shutil
 import sys
 
+
 class hg(object):
 
     _Config = {}
@@ -26,132 +29,142 @@ class hg(object):
 
     def __init__(self, downloads=2):
         self._NUM_CONCURRENT_DOWNLOADS = downloads
-        Path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../")
-        self._Processors = self.LoadModules(Path, "processors")
-        self._Feeds = self.LoadModules(Path, "feeds")
+        Path = \
+            os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                         '../')
+        self._Processors = self.LoadModules(Path, 'processors')
+        self._Feeds = self.LoadModules(Path, 'feeds')
 
         # Configure the logging
-        logging.basicConfig(level=logging.INFO,
-                            format='%(asctime)s %(levelname)-8s %(message)s',
-                            datefmt='%Y-%m-%d %H:%M:%S',
-                            stream=sys.stdout)
-        # Configure requests to show only WARNING messages
-        logging.getLogger("requests").setLevel(logging.WARNING)
-        logging.getLogger("HG").setLevel(logging.WARNING)
-        logging.addLevelName( logging.WARNING, "\033[1;31m%s\033[1;0m" % logging.getLevelName(logging.WARNING))
-        logging.addLevelName( logging.ERROR, "\033[1;41m%s\033[1;0m" % logging.getLevelName(logging.ERROR))
 
-        fconf = os.path.join(Path, "conf/hg.conf")
+        logging.basicConfig(level=logging.INFO,
+                            format='%(asctime)s %(levelname)-8s %(message)s'
+                            , datefmt='%Y-%m-%d %H:%M:%S',
+                            stream=sys.stdout)
+
+        # Configure requests to show only WARNING messages
+
+        logging.getLogger('requests').setLevel(logging.WARNING)
+        logging.getLogger('HG').setLevel(logging.WARNING)
+        logging.addLevelName(logging.WARNING, "\033[1;31m%s\033[1;0m"
+                             % logging.getLevelName(logging.WARNING))
+        logging.addLevelName(logging.ERROR, "\033[1;41m%s\033[1;0m"
+                             % logging.getLevelName(logging.ERROR))
+
+        fconf = os.path.join(Path, 'conf/hg.conf')
         if os.path.exists(fconf):
             cfg = ConfigParser.ConfigParser()
             cfg.read(fconf)
 
             VX = {}
-            VX["Enabled"]= cfg.get('vxcage', 'enabled')
-            VX["connection"] = cfg.get('vxcage', 'connection');
+            VX['Enabled'] = cfg.get('vxcage', 'enabled')
+            VX['connection'] = cfg.get('vxcage', 'connection')
 
             Viper = {}
-            Viper["Enabled"] = cfg.get('viper', 'enabled')
-            Viper["connection"] = cfg.get('viper', 'connection');
+            Viper['Enabled'] = cfg.get('viper', 'enabled')
+            Viper['connection'] = cfg.get('viper', 'connection')
 
             Cuckoo = {}
-            Cuckoo["Enabled"]= cfg.get('cuckoo', 'enabled')
-            Cuckoo["connection"] = cfg.get('cuckoo', 'connection');
+            Cuckoo['Enabled'] = cfg.get('cuckoo', 'enabled')
+            Cuckoo['connection'] = cfg.get('cuckoo', 'connection')
 
             Mongo = {}
-            Mongo["Enabled"]= cfg.get('mongodb', 'enabled')
-            Mongo["host"] = cfg.get('mongodb', 'host');
-            Mongo["port"] = cfg.get('mongodb', 'port');
+            Mongo['Enabled'] = cfg.get('mongodb', 'enabled')
+            Mongo['host'] = cfg.get('mongodb', 'host')
+            Mongo['port'] = cfg.get('mongodb', 'port')
 
-            self._Config["VxCage"] = VX
-            self._Config["Cuckoo"] = Cuckoo
-            self._Config["Mongo"] = Mongo
-            self._Config["Viper"] = Viper
+            self._Config['VxCage'] = VX
+            self._Config['Cuckoo'] = Cuckoo
+            self._Config['Mongo'] = Mongo
+            self._Config['Viper'] = Viper
 
-        logging.info("Initializing HG")
+        logging.info('Initializing HG')
 
     def LoadModules(self, Root, Directory):
         Modules = []
         Walk_In = os.path.join(Root, Directory)
-        for arg, dirname, names in os.walk(Walk_In):
-            for name in fnmatch.filter(names, "*.py"):
-                if name != "__init__.py":
-                    name = "%s.%s" % (Directory, name.split(".py")[0])
-                    processor = __import__(name, globals(), locals(), [''])
-                    components = name.split(".")[1:]
+        for (arg, dirname, names) in os.walk(Walk_In):
+            for name in fnmatch.filter(names, '*.py'):
+                if name != '__init__.py':
+                    name = '%s.%s' % (Directory, name.split('.py')[0])
+                    processor = __import__(name, globals(), locals(),
+                            [''])
+                    components = name.split('.')[1:]
                     for com in components:
                         class_ = getattr(processor, com)
                         Modules.append(class_())
         return Modules
 
     def SaveFile(self, data):
-        if len(data["filename"]) <= 0:
-            logging.debug("Not a valid filename")
+        if len(data['filename']) <= 0:
+            logging.debug('Not a valid filename')
             print data
             sys.exit(-1)
         tempd = tempfile.mkdtemp()
-        path = os.path.join(tempd, data["filename"])
-        fp = open(path, "wb")
-        fp.write(data["content"])
+        path = os.path.join(tempd, data['filename'])
+        fp = open(path, 'wb')
+        fp.write(data['content'])
         fp.close()
         return path
 
     def GetFileType(self, data):
         filetype = None
         try:
+
             # using python-magic from apt-get(Ubuntu)
+
             ms = magic.open(magic.MAGIC_MIME)
             ms.load()
             filetype = ms.buffer(data)
         except:
             try:
+
                 # from pip install libmagic, python-magic
+
                 filetype = magic.Magic(mime=True).from_buffer(data)
-            except Exception as e:
+            except Exception, e:
                 logging.error(e)
                 self._End_Process = True
 
-        return filetype.split(";")[0]
-
+        return filetype.split(';')[0]
 
     def Fetch(self, URL):
         try:
-            headers = {'User-Agent':'Mozilla/5.0'}
-            re = requests.get(URL,
-                              allow_redirects=True,
-                              timeout=100,
+            headers = {'User-Agent': 'Mozilla/5.0'}
+            re = requests.get(URL, allow_redirects=True, timeout=100,
                               headers=headers)
             if re.status_code == 200:
                 UseDisp = False
                 for item in re.headers:
-                    if item.lower() == "content-disposition":
+                    if item.lower() == 'content-disposition':
                         UseDisp = True
                 if not UseDisp:
-                    filename = URL.split('/')[-1].split('#')[0].split('?')[0]
+                    filename = URL.split('/')[-1].split('#'
+                            )[0].split('?')[0]
                     if filename == None:
                         filename = URL.split('/')
                 else:
                     Disposition = re.headers['Content-Disposition']
                     try:
-                        filename = Disposition.split('filename=')[1].split('"')[1]
+                        filename = Disposition.split('filename='
+                                )[1].split('"')[1]
                     except Exception:
-                        filename = Disposition.split('filename=')[1].split('"')[0]
+                        filename = Disposition.split('filename='
+                                )[1].split('"')[0]
 
                 if len(filename) <= 0:
                     logging.debug(URL)
                     sys.exit(-1)
 
-                return dict({"filename":filename,
-                             "content": re.content,
-                             "status_code": re.status_code})
+                return dict({'filename': filename,
+                            'content': re.content,
+                            'status_code': re.status_code})
             else:
-                return dict({"filename": None,
-                             "content": None,
-                             "status_code": re.status_code})
+                return dict({'filename': None, 'content': None,
+                            'status_code': re.status_code})
         except Exception, e:
-            return dict({"filename": None,
-                         "content": None,
-                         "status_code": "0"}) # 0 for timeout
+            return dict({'filename': None, 'content': None,
+                        'status_code': '0'})  # 0 for timeout
         except KeyboardInterrupt:
             pass
 
@@ -162,19 +175,15 @@ class hg(object):
             except Queue.Empty:
                 continue
             data = self.Fetch(url)
-            #message = "Status: "+str(data["status_code"])+" URL: "+ url[0:50]
-            if data["status_code"] == 200:
+
+            # message = "Status: "+str(data["status_code"])+" URL: "+ url[0:50]
+
+            if data['status_code'] == 200:
                 logging.info(url)
-            '''
-            if data["status_code"] != 200:
-                logging.warning(message)
-            else:
-                logging.info(message)
-            '''
             if data['content'] != None:
                 fpath = self.SaveFile(data)
                 ftype = self.GetFileType(data['content'])
-                if (os.path.getsize(fpath) > 0):
+                if os.path.getsize(fpath) > 0:
                     MD5 = hashlib.md5(data['content']).hexdigest()
                     for proc in self._Processors:
                         proc.run(self._Config, MD5, fpath, ftype)
@@ -186,7 +195,7 @@ class hg(object):
     def run(self):
         threads = []
 
-        logging.info("Initializing Downloaders")
+        logging.info('Initializing Downloaders')
         for unused_index in range(self._NUM_CONCURRENT_DOWNLOADS):
             thread = threading.Thread(target=self.Downloader)
             thread.daemon = True
@@ -196,19 +205,23 @@ class hg(object):
         while not self._End_Process:
             try:
                 if self._Fila.qsize() > 300:
-                    logging.info("Status: %s files to download", self._Fila.qsize())
-                    logging.info("Status: Feeds waiting 5 minute")
+                    logging.info('Status: %s files to download',
+                                 self._Fila.qsize())
+                    logging.info('Status: Feeds waiting 5 minute')
                     time.sleep(60 * 5)
                     continue
                 for feed in self._Feeds:
                     try:
                         feed.run(self._Fila)
-                    except Exception as e:
-                        logging.error("Feed with errors: %s", feed.Name)
+                    except Exception, e:
+                        logging.error('Feed with errors: %s', feed.Name)
                         logging.error(e)
                         sys.exit(-1)
-                logging.info("Status: Feeds retrieved, waiting 30 minutes")
+                logging.info('Status: Feeds retrieved, waiting 30 minutes'
+                             )
                 time.sleep(60 * 30)
             except KeyboardInterrupt:
-                logging.info("Shutdown HG")
+                logging.info('Shutdown HG')
                 self._End_Process = True
+
+
--- hg/core/feeds.py
+++ hg/core/feeds.py
@@ -1,6 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import logging
 import urllib2
 from lxml import etree
+
 
 class Feeds(object):
 
@@ -8,11 +12,13 @@ class Feeds(object):
         '''
         :description : Execute download from a URL
         '''
+
         try:
-            head = {"User-agent":"Mozilla/5.0 (X11; U; Linux i686)"}
+            head = {'User-agent': 'Mozilla/5.0 (X11; U; Linux i686)'}
             r = urllib2.Request(URL, headers=head)
             return urllib2.urlopen(r)
         except Exception, e:
-            logging.error("Failure, %s" % e)
+            logging.error('Failure, %s' % e)
             return None
+
 
