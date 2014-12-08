db09b13cda869f764e84b5a1434ab2d6321a09a9
Neriberto C.Prado <neriberto.prado@bluepex.com>
2014-12-08 16:33:14 -0200

    Sending to viper without check if file already exists

--- processors/viper.py
+++ processors/viper.py
@@ -27,8 +27,8 @@ class viper(object):
             if CONFIG['Viper']['Enabled'] == 'yes':
                 if FILETYPE in self.TYPES:
                     viper_conn = CONFIG['Viper']['connection']
-                    if self.not_exist(MD5, viper_conn):
-                        return self.add(viper_conn, FULLPATH)
+                    #if self.not_exist(MD5, viper_conn):
+                    return self.add(viper_conn, FULLPATH)
         except Exception, e:
             logging.error('Viper:run %s' % e)
 
