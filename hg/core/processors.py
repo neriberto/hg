b3f47ef1eaed9e60efe379537ece0272a00163ec
Neriberto C.Prado <neriberto.prado@bluepex.com>
2017-04-04 15:06:52 -0300

    Fix bug in base class for processors

--- hg/core/processors.py
+++ hg/core/processors.py
@@ -10,34 +10,39 @@ class Processors(object):
 
     Name = ""
     ADDURL = '%stasks/create/file'
-    __TYPES = []
+    TYPES = []
 
-    def __init(self):
+    def __init__(self):
         self.TYPES.append('application/x-dosexec')
         self.TYPES.append('application/x-executable')
         self.TYPES.append('application/pdf')
         self.TYPES.append('application/msword')
         self.TYPES.append('application/octect-stream')
+        logging.info('Processor %s ready', self.Name)
 
     def run(self, CONFIG, MD5, FULLPATH, FILETYPE):
         try:
             if CONFIG[self.Name]['Enabled'] == 'yes':
                 if FILETYPE in self.TYPES:
                     connection = CONFIG[self.Name]['connection']
-                    if self.not_exist(MD5, connection):
-                        return self.add(connection, FULLPATH)
+                    # if self.not_exist(MD5, connection):
+                    return self.add(connection, FULLPATH)
         except Exception as ex:
             logging.error('%s:run %s' % self.Name, ex)
 
     def add(self, URL, fullpath):
+        logging.info('Submitting file %s to %s', fullpath, URL)
         try:
             arquivo = os.path.basename(fullpath)
-            Files = {'file': (arquivo, open(fullpath, 'rb'))}
-            r = requests.post(self.format_url(URL), files=Files)
-            return r.status_code == 200
+            my_files = {'file': (arquivo, open(fullpath, 'rb'))}
+            req = requests.post(self.format_url(URL), files=my_files)
+            return req.status_code == 200
         except Exception as ex:
             logging.error('%s:add %s', self.Name, ex)
             return False
 
     def format_url(self, URL):
         return self.ADDURL % URL
+
+    def not_exist(self, MD5, connection):
+        return None
