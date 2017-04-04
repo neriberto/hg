937bd1a838b88fcbf92c623b72e5ae22fe718886
Neriberto C.Prado <neriberto.prado@bluepex.com>
2017-04-04 15:06:20 -0300

    Added docstring to class viper

--- processors/viper.py
+++ processors/viper.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2013, Neriberto C.Prado
-#
-# This file is part of HG - https://github.com/neriberto/hg
-# See the file 'docs/LICENSE' for copying permission.
-
 from hg.core.processors import Processors
 import logging
 import requests
 
 
 class viper(Processors):
+    '''
+    Process submissions to Viper Framework
+    '''
 
     Name = 'Viper'
     ADDURL = '%sfile/add'
 
     def not_exist(self, MD5, URL):
+        '''
+        Verify if a file already exists in Viper
+        '''
         try:
-            Data = {'md5': MD5}
-            r = requests.post('%sfile/find' % URL, data=Data)
-            return r.status_code == 404
-        except Exception as e:
-            logging.error('Viper:not_exist %s' % e)
+            req = requests.post('%sfile/find' % URL, data={'md5': MD5})
+            return req.status_code == 404
+        except Exception as ex:
+            logging.error('Viper:not_exist %s', ex)
             return False
